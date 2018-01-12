1ab753ec18fe2bff44d88d5b145b0bccd7115fb5
Nanette Girzi <ngirzi@gmail.com>
2018-01-12 13:07:58 -0800

    fix java bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
-  $("form#insurance").submit(function(event) {
+  $("#form").submit(function(event) {
     var language = $("select#language").val();
     var size = $("select#size").val();
     var application = $("select#application").val();
     var android = $("select#android").val();
     var language = $("select#android").val();
+
+    if (language === "C#/.Net" && size === "large company "&& android === "no") {
+      $("#csharp").show();
+      $("#form").hide();
+    } else {
+       $("#Java").show();
+       $("form").hide();
+     }
+
+   
+
+
+
+
+     event.preventDefault();
+
+
+    });
+  });
