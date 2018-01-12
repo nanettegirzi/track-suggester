8cdfc418d70fd7c406ba2c263c3d26d6bdc606c3
Nanette Girzi <ngirzi@gmail.com>
2018-01-12 11:46:37 -0800

    add js

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $("form#insurance").submit(function(event) {
+    var language = $("select#language").val();
+    var size = $("select#size").val();
+    var application = $("select#application").val();
+    var android = $("select#android").val();
+    var language = $("select#android").val();
